2f7f861192caf73d12116c4019ffadda93b3e369
marcocmm <marco.cmm@gmail.com>
2018-07-24 16:54:29 -0300

    nodemailer removido

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var nodemailer = require('nodemailer');
 
 var app = express();
 
